eff7838b5af0e1514d93aa8c6bb6f2a94f3660d3
Dustin <ldust667@gmail.com>
2016-01-12 10:57:20 -0500

    Improved backup

--- homebackup.sh
+++ homebackup.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 #this script is used to backup the users home directory
-cd /home
 
-tar -cf /var/tmp/homebackup.tar $USER
-
-rm /var/tmp/homebackup.tar.gz
-
-gzip /var/tmp/homebackup.tar
-
-scp homebackup.tar.gz administrator@10.1.10.74:~
-
-echo "Job Completed"
+echo "Valid home folders:"
+ls /home
 echo
+echo "Which home folder would you like to backup?:"
+read HOMEFOLDER
+
+BACKUPDIR=/home
+TIMESTAMP=$(date | awk '{ print $1 "_" $2"_"$3"_"$6 }')
+TARFILENAME="/var/tmp/$HOMEFOLDER.$TIMESTAMP.tar"
+GZIPFILENAME="/var/tmp/$TARFILENAME.gz"
+REMOTESERVER="10.1.10.74"
+REMOTEUSER="administrator"
+REMOTEDIR="~"
+LOGFILE="/var/tmp/homebackuplog.txt"
+
+cd $BACKUPDIR
+tar -cf $TARFILENAME $HOMEFOLDER
+gzip $TARFILENAME
+rsync $GZIPFILENAME $REMOTEUSER@$REMOTERSERVER:$REMOTEDIR
+date >> $LOGFILE
+echo "Backup Complete"
+
+
+
+
